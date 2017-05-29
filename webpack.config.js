e8a3097c14f7aa0662492d97a252acc51bf92c52
tjscratch <p.malyshev@inna.ru>
2017-05-29 11:28:00 +0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+import path from 'path';
+import webpack from 'webpack';
+
+export default {
+    // devtools: 'eval-source-map',
+    entry: [
+        'webpack-hot-middleware/client',
+        path.join(__dirname, '/client/index.js'),
+        ],
+    output: {
+        path: '/',
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    plugins: [
+        // new webpack.NoErrorsPlugin(),
+        // new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                include: path.join(__dirname, 'client'),
+                loaders: ['react-hot-loader', 'babel-loader']
+            }
+        ]
+    },
+}
