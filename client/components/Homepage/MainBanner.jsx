67213ea90fb5da4262fef22f38728d9d4ffb9e79
tjscratch <p.malyshev@inna.ru>
2017-06-13 17:10:25 +0300

    banner

--- client/components/Homepage/MainBanner.jsx
+++ client/components/Homepage/MainBanner.jsx
@@ -18,7 +18,7 @@ export class MainBanner extends React.Component{
         getBanner().then(results => {
             this.setState({
                 // banner:  `url(${results.data.Slider[0].Image}) center/cover no-repeat`
-                banner:  `url(http://www.womenzone.org/wp-content/uploads/2016/07/leto-3.jpg) center/cover no-repeat`
+                banner:  `url(http://www.kefaloniapress.gr/wp-content/uploads/2016/10/%CE%9A%CE%91%CE%A4%CE%91%CE%99%CE%93%CE%99%CE%94%CE%95%CE%A3-%CE%91%CE%9D%CE%95%CE%9C%CE%9F%CE%99.jpg) center/cover no-repeat`
 
             })
         })
